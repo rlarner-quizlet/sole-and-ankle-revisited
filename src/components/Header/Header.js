e86aa31d59cf15577f2ac72c1f878a77c7de3ac9
Ross Larner <ross.larner@quizlet.com>
2023-01-19 08:31:06 -0800

    header spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -56,9 +56,9 @@ const Header = () => {
 
 const MainHeader = styled.div`
   display: flex;
+  overflow: auto;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   border-top: 4px solid ${COLORS.gray[900]};
 
@@ -70,7 +70,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndDown} {
